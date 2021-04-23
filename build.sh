ab5b01929c00da2046546cd5064c1ab8cdb47582
ArGintum <81860007+ArGintum@users.noreply.github.com>
2021-04-23 17:38:15 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ lst=$(python3-config --includes)
 arr=($lst)
 mkdir objs
 g++ -g -fPIC ${arr[0]} -o ./objs/CliqueEnumerator.o -c cliques-1.cpp
-g++ -fPIC -g -shared -o ./lib/CliqueEnumerator.so ./objs/CliqueEnumerator.o -lboost_python3 -lboost_numpy3
+g++ -fPIC -g -shared -o CliqueEnumerator.so ./objs/CliqueEnumerator.o -lboost_python3 -lboost_numpy3
