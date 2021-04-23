ea308437b1c0113492af8164564a7a27df8fd4f9
ArGintum <81860007+ArGintum@users.noreply.github.com>
2021-04-23 17:57:18 +0300

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-
-lst=$(python3-config --includes)
-arr=($lst)
-mkdir objs
-g++ -g -fPIC ${arr[0]} -o ./objs/CliqueEnumerator.o -c cliques-1.cpp
-g++ -fPIC -g -shared -o CliqueEnumerator.so ./objs/CliqueEnumerator.o -lboost_python3 -lboost_numpy3
